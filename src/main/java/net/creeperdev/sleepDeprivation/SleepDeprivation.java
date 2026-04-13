48f3f3f14db281ad89d08326ef0636915ae43905
judah <judah.violinmaster@gmail.com>
2026-04-13 12:54:27 -0400

    added better server config handling

--- src/main/java/net/creeperdev/sleepDeprivation/SleepDeprivation.java
+++ src/main/java/net/creeperdev/sleepDeprivation/SleepDeprivation.java
@@ -38,35 +38,199 @@ public class SleepDeprivation implements ModInitializer {
                     FigManager.load();
 
                     Figs f = FigManager.FIGS;
-                    {
+                    int valid = 0;
+                    int invalid = 0;
+                    if (payload.interval() >= 1) {
                         f.interval = payload.interval();
-                        f.intervalRandomness = payload.intervalRandomness();
-                        f.modifyInventory = payload.modifyInventory();
-                        f.stageThreshold1 = payload.stageThreshold1();
-                        f.stageThreshold2 = payload.stageThreshold2();
-                        f.stageThreshold3 = payload.stageThreshold3();
-                        f.stageThreshold4 = payload.stageThreshold4();
-                        f.stageThreshold5 = payload.stageThreshold5();
-                        f.enableStageMessage = payload.enableStageMessage();
-                        f.stageMessage1 = payload.stageMessage1();
-                        f.stageMessage2 = payload.stageMessage2();
-                        f.stageMessage3 = payload.stageMessage3();
-                        f.stageMessage4 = payload.stageMessage4();
-                        f.stageMessage5 = payload.stageMessage5();
-                        f.inventorySwapMultiplier1 = payload.inventorySwapMultiplier1();
-                        f.inventorySwapMultiplier2 = payload.inventorySwapMultiplier2();
-                        f.inventorySwapMultiplier3 = payload.inventorySwapMultiplier3();
-                        f.inventorySwapMultiplier4 = payload.inventorySwapMultiplier4();
-                        f.inventorySwapMultiplier5 = payload.inventorySwapMultiplier5();
-                        f.includeHotbar = payload.includeHotbar();
-                        f.modifyEffects = payload.modifyEffects();
-                        f.potency1 = payload.potency1();
-                        f.potency2 = payload.potency2();
-                        f.potency3 = payload.potency3();
-                        f.potency4 = payload.potency4();
-                        f.potency5 = payload.potency5();
-                        FigManager.save();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: interval"));
+                        logger.error("Malformed value: interval");
                     }
+
+                    if (payload.intervalRandomness() >= 0) {
+                        f.intervalRandomness = payload.intervalRandomness();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: intervalRandomness"));
+                        logger.error("Malformed value: intervalRandomness");
+                    }
+
+                    if (payload.stageThreshold1() >= 0) {
+                        f.stageThreshold1 = payload.stageThreshold1();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageThreshold1"));
+                        logger.error("Malformed value: stageThreshold1");
+                    }
+                    if (payload.stageThreshold2() >= 0) {
+                        f.stageThreshold2 = payload.stageThreshold2();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageThreshold2"));
+                        logger.error("Malformed value: stageThreshold2");
+                    }
+                    if (payload.stageThreshold3() >= 0) {
+                        f.stageThreshold3 = payload.stageThreshold3();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageThreshold3"));
+                        logger.error("Malformed value: stageThreshold3");
+                    }
+                    if (payload.stageThreshold4() >= 0) {
+                        f.stageThreshold4 = payload.stageThreshold4();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageThreshold4"));
+                        logger.error("Malformed value: stageThreshold4");
+                    }
+                    if (payload.stageThreshold5() >= 0) {
+                        f.stageThreshold5 = payload.stageThreshold5();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageThreshold5"));
+                        logger.error("Malformed value: stageThreshold5");
+                    }
+
+                    if (payload.inventorySwapMultiplier1() >= 0) {
+                        f.inventorySwapMultiplier1 = payload.inventorySwapMultiplier1();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: inventorySwapMultiplier1"));
+                        logger.error("Malformed value: inventorySwapMultiplier1");
+                    }
+                    if (payload.inventorySwapMultiplier2() >= 0) {
+                        f.inventorySwapMultiplier2 = payload.inventorySwapMultiplier2();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: inventorySwapMultiplier2"));
+                        logger.error("Malformed value: inventorySwapMultiplier2");
+                    }
+                    if (payload.inventorySwapMultiplier3() >= 0) {
+                        f.inventorySwapMultiplier3 = payload.inventorySwapMultiplier3();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: inventorySwapMultiplier3"));
+                        logger.error("Malformed value: inventorySwapMultiplier3");
+                    }
+                    if (payload.inventorySwapMultiplier4() >= 0) {
+                        f.inventorySwapMultiplier4 = payload.inventorySwapMultiplier4();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: inventorySwapMultiplier4"));
+                        logger.error("Malformed value: inventorySwapMultiplier4");
+                    }
+                    if (payload.inventorySwapMultiplier5() >= 0) {
+                        f.inventorySwapMultiplier5 = payload.inventorySwapMultiplier5();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: inventorySwapMultiplier5"));
+                        logger.error("Malformed value: inventorySwapMultiplier5");
+                    }
+
+                    if (payload.stageMessage1() != null && payload.stageMessage1().length() < 512) {
+                        f.stageMessage1 = payload.stageMessage1();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageMessage1"));
+                        logger.error("Malformed value: stageMessage1");
+                    }
+                    if (payload.stageMessage2() != null && payload.stageMessage2().length() < 512) {
+                        f.stageMessage2 = payload.stageMessage2();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageMessage2"));
+                        logger.error("Malformed value: stageMessage2");
+                    }
+                    if (payload.stageMessage3() != null && payload.stageMessage3().length() < 512) {
+                        f.stageMessage3 = payload.stageMessage3();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageMessage3"));
+                        logger.error("Malformed value: stageMessage3");
+                    }
+                    if (payload.stageMessage4() != null && payload.stageMessage4().length() < 512) {
+                        f.stageMessage4 = payload.stageMessage4();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageMessage4"));
+                        logger.error("Malformed value: stageMessage4");
+                    }
+                    if (payload.stageMessage5() != null && payload.stageMessage5().length() < 512) {
+                        f.stageMessage5 = payload.stageMessage5();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: stageMessage5"));
+                        logger.error("Malformed value: stageMessage5");
+                    }
+
+                    if (payload.potency1() >= 0 && payload.potency1() <= 10) {
+                        f.potency1 = payload.potency1();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: potency1"));
+                        logger.error("Malformed value: potency1");
+                    }
+                    if (payload.potency2() >= 0 && payload.potency2() <= 10) {
+                        f.potency2 = payload.potency2();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: potency2"));
+                        logger.error("Malformed value: potency2");
+                    }
+                    if (payload.potency3() >= 0 && payload.potency3() <= 10) {
+                        f.potency3 = payload.potency3();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: potency3"));
+                        logger.error("Malformed value: potency3");
+                    }
+                    if (payload.potency4() >= 0 && payload.potency4() <= 10) {
+                        f.potency4 = payload.potency4();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: potency4"));
+                        logger.error("Malformed value: potency4");
+                    }
+                    if (payload.potency5() >= 0 && payload.potency5() <= 10) {
+                        f.potency5 = payload.potency5();
+                        valid++;
+                    } else {
+                        invalid++;
+                        context.player().sendSystemMessage(Component.literal("Malformed value: potency5"));
+                        logger.error("Malformed value: potency5");
+                    }
+                    f.modifyInventory = payload.modifyInventory();
+                    valid++;
+                    f.includeHotbar = payload.includeHotbar();
+                    valid++;
+                    f.modifyEffects = payload.modifyEffects();
+                    valid++;
+                    f.enableStageMessage = payload.enableStageMessage();
+                    valid++;
+                    FigManager.save();
+
                     for (ServerPlayer player : context.server().getPlayerList().getPlayers()) {
                         ServerPlayNetworking.send(player,new FigPacket(
                             f.interval,
@@ -98,7 +262,8 @@ public class SleepDeprivation implements ModInitializer {
                         ));
                     }
                     logger.warn("Figs for sleep_deprivation were modified by " + context.player().getPlainTextName());
-                    context.player().sendSystemMessage(Component.literal("Updated Figs for sleep_deprivation successfully"));
+                    logger.warn(valid + " succeeded, " + invalid + " failed.");
+                    context.player().sendSystemMessage(Component.literal("Updated Figs for sleep_deprivation" +". " + valid + " succeeded, " + invalid + " failed."));
                 } else {
                     logger.error(context.player().getPlainTextName() + " attempted to modify figs without permission!");
                     context.player().sendSystemMessage(Component.literal("Failed to update figs, insufficient permissions"));
